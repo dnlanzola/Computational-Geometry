86ea9bbc0cbac058d56c536a8a27426ccb58054f
Brandon Jones <bjones15@mail.usf.edu>
2019-11-20 17:11:31 -0500

    Update Curve.py

--- Python/Curve.py
+++ Python/Curve.py
@@ -1,9 +1,9 @@
 class Curve:
     # setting parameter for the class
-    def __init__(self, ID, value, val):
+    def __init__(self, ID, value):
         self.ID = ID
         self.value = value
-        self.val = val
+
 
 
 
